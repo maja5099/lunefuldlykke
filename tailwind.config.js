6706c4828ada652665f71fdf86e42e83ef56a482
Maja Sørensen <majabirchsorensen@gmail.com>
2022-04-05 16:20:45 +0200

    bg-hero doesnt work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url('src/content/lunefuld-lykke-header.jpg')",
+      }
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
